c34952f9cc546527e57f82d97fb05473adb4ecbc
Lycoiref <lycoiref@qq.com>
2024-10-18 00:54:15 +0800

    feat: 微调样式

--- packages/platform-core/src/app/auth/page.tsx
+++ packages/platform-core/src/app/auth/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 
 const LineInput = ({ placeholder }: { placeholder: string }) => {
@@ -13,34 +14,32 @@ const LineInput = ({ placeholder }: { placeholder: string }) => {
 const Auth = () => {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
-			<div className="bg fixed right-[200px] top-40 text-[400px] -z-10">
+			{/* <div className="bg fixed right-[200px] top-40 text-[400px] -z-10">
 				Hello
 			</div>
 			<div className="bg fixed right-[600px] bottom-40 text-[400px] -z-10">
 				World
-			</div>
+			</div> */}
 			<Image
 				width={400}
-				height={600}
+				height={400}
 				className="fixed left-0 bottom-0"
 				src="/Kotone_Fujita.webp"
 				alt="琴音"
 			/>
-			<div className="login-box w-[750px] h-[450px] flex items-center rounded overflow-hidden shadow-xl hover:shadow-2xl hover:scale-[102%] transition-all z-10">
-				<div className="left h-full">
+			<div className="login-box w-[750px] h-[450px] flex items-center bg-white rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl hover:scale-[102%] transition-all z-10">
+				<div className="left h-full rounded-2xl overflow-hidden shadow-2xl">
 					<Image
 						width={300}
 						height={400}
-						// layout="responsive"
 						src="/美游.jpg"
 						alt="美游"
 						style={{
-							width: 'auto',
 							height: '100%',
 						}}
 					/>
 				</div>
-				<div className="right flex flex-col justify-center flex-grow h-full items-center gap-4 px-8 bg-white">
+				<div className="right flex flex-col justify-center flex-grow h-full items-center gap-4 px-8">
 					<div className="self-start text-2xl mb-8 ml-10 font-black">
 						Login
 					</div>
@@ -50,7 +49,7 @@ const Auth = () => {
 						<a href="/register">忘记密码？</a>
 						<a href="/register">注册账号</a>
 					</div>
-					<button className="bg-black text-white rounded p-2 w-3/5">
+					<button className="bg-black text-white rounded p-2 w-3/5 hover:shadow-2xl transition-all">
 						登录
 					</button>
 				</div>
